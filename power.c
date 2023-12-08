62f348ebe8c09cc83ee44592cf40c3c2629a2df3
DidiOmah <152418885+DidiOmah@users.noreply.github.com>
2023-12-08 22:43:06 +0100

    power.c
    Calculate square, cube, and fourth power of variables.

---
+++ power.c
@@ -0,0 +1,19 @@
+/* Area of square,Volume of cube,Fourth power of variables*/
+#include<stdio.h>
+#include<conio.h>
+void main()
+{	clrscr();
+	int x;
+	float a,l,b,h,ar,r,P,V,c,S,TSA,CSA,Q;
+	printf(" \npress 1 to calculate area of square  ");
+	printf(" \n\npress 2 to calculate area of sector of circle  ");
+	printf(" \n\npress 3 to calculate area of rectangle  ");
+	printf(" \n\npress 4 to calculate area of triangle  ");
+	printf(" \n\npress 5 to calculate perimeter of rectangle  ");
+	printf(" \n\npress 6 to calculate perimeter of square  ");
+	printf(" \n\npress 7 to calculate circumference of circle  ");
+	printf(" \n\npress 8 to calculate volume of cuboid  ");
+	printf(" \n\npress 9 to calculate surface area of cuboid  ");
+	printf(" \n\npress 10 to calculate volume of cube\n");
+	scanf("%d",&x);
+	switch(x)
